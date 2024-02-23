8cc144653919d54ef478377238b8dd23d8d75f45
Okamoto <p41l4w3v@s.okayama-u.ac.jp>
2024-02-23 15:28:58 +0900

    add

---
+++ esp32/motor_test/motor_test.ino
@@ -0,0 +1,25 @@
+#define MOTOR1_IN1 13
+#define MOTOR1_IN2 12
+#define MOTOR2_IN1 14
+#define MOTOR2_IN2 27
+
+void setup() {
+  pinMode(MOTOR1_IN1, OUTPUT);
+  pinMode(MOTOR1_IN2, OUTPUT);
+  pinMode(MOTOR2_IN1, OUTPUT);
+  pinMode(MOTOR2_IN2, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(MOTOR1_IN1, HIGH);
+  digitalWrite(MOTOR1_IN2, LOW);
+  digitalWrite(MOTOR2_IN1, HIGH);
+  digitalWrite(MOTOR2_IN2, LOW);
+  delay(5000);
+
+  digitalWrite(MOTOR1_IN1, LOW);
+  digitalWrite(MOTOR1_IN2, HIGH);
+  digitalWrite(MOTOR2_IN1, LOW);
+  digitalWrite(MOTOR2_IN2, HIGH);
+  delay(5000);
+}
