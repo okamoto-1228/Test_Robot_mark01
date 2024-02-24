934ebb400ea32dd5fb6bf6e17d9057485620ba07
Okamoto <p41l4w3v@s.okayama-u.ac.jp>
2024-02-24 15:42:45 +0900

    add

--- esp32/micro_ros_test.repos/micro_ros_test.repos.ino
+++ esp32/micro_ros_test.repos/micro_ros_test.repos.ino
@@ -4,7 +4,6 @@
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-
 #include <geometry_msgs/msg/twist.h>
 
 rcl_subscription_t subscriber;
@@ -19,7 +18,6 @@ rcl_node_t node;
 #define RIGHT_MOTOR_IN2 12
 #define LEFT_MOTOR_IN1 14
 #define LEFT_MOTOR_IN2 27
-
 #define RIGHT_ENABLE 26
 #define LEFT_ENABLE 25
 
@@ -36,8 +34,8 @@ rcl_node_t node;
     if ((temp_rc != RCL_RET_OK)) { error_loop(); } \
   }
 
-
-
+#define TREAD 0.1 //トレッド長 [m]
+#define SPEED 1 //速度
 
 // PWM制御の設定
 const int freq = 30000;
@@ -55,9 +53,56 @@ void error_loop() {
 
 //twist message cb
 void subscription_callback(const void *msgin) {
-  const geometry_msgs__msg__Twist *msg = (const geometry_msgs__msg__Twist *)msgin;
+  // duty比
+  int right_duty = 0;
+  int left_duty = 0;
+
+  const geometry_msgs__msg__Twist *vel = (const geometry_msgs__msg__Twist *)msgin;
   // if velocity in x direction is 0 turn off LED, if 1 turn on LED
-  digitalWrite(LED_PIN, (msg->linear.x == 0) ? LOW : HIGH);
+  digitalWrite(LED_PIN, (vel->linear.x == 0) ? LOW : HIGH);
+
+  right_duty += (vel->linear.x + vel->angular.z * TREAD / 2) * 255 / SPEED;
+  left_duty += (vel->linear.x - vel->angular.z * TREAD / 2) * 255 / SPEED;
+
+  // duty比の調整
+  if (right_duty > 255) right_duty = 255;
+  else if (right_duty < -255) right_duty = -255;
+  if (left_duty > 255) left_duty = 255;
+  else if (left_duty < -255) left_duty = -255;
+
+  Serial.print("right_duty: ");
+  Serial.println(right_duty);
+  Serial.print("left_duty: ");
+  Serial.println(left_duty);
+
+  // 回転
+  if (right_duty > 0) {  //前方向
+    digitalWrite(RIGHT_MOTOR_IN1, HIGH);
+    digitalWrite(RIGHT_MOTOR_IN2, LOW);
+    ledcWrite(rightpwmChannel, right_duty);
+  } else if (right_duty < 0) {  //後ろ方向
+    digitalWrite(RIGHT_MOTOR_IN1, LOW);
+    digitalWrite(RIGHT_MOTOR_IN2, HIGH);
+    ledcWrite(rightpwmChannel, right_duty);
+  } else {  //モーター停止
+    digitalWrite(RIGHT_MOTOR_IN1, LOW);
+    digitalWrite(RIGHT_MOTOR_IN2, LOW);
+    ledcWrite(rightpwmChannel, 0);
+  }
+
+  if (left_duty > 0) {  //前方向
+    digitalWrite(LEFT_MOTOR_IN1, HIGH);
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
+    ledcWrite(leftpwmChannel, left_duty);
+  } else if (left_duty < 0) {  //後ろ方向
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    digitalWrite(LEFT_MOTOR_IN2, HIGH);
+    ledcWrite(leftpwmChannel, left_duty);
+  } else {  //モーター停止
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
+    ledcWrite(leftpwmChannel, 0);
+  }
 }
 
 void setup() {
@@ -75,13 +120,9 @@ void setup() {
   ledcSetup(rightpwmChannel, freq, resolution);
   ledcSetup(leftpwmChannel, freq, resolution);
 
-
-
-
   // PWM制御でモーターをアタッチ
   ledcAttachPin(RIGHT_ENABLE, rightpwmChannel);
   ledcAttachPin(LEFT_ENABLE, leftpwmChannel);
-
 
   //シリアルモニターに表示
   Serial.begin(115200);
@@ -92,7 +133,6 @@ void setup() {
   delay(2000);
 
   allocator = rcl_get_default_allocator();
-
   //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
   // create node
@@ -117,11 +157,11 @@ void loop() {
   digitalWrite(RIGHT_MOTOR_IN2, LOW);
   digitalWrite(LEFT_MOTOR_IN1, LOW);
   digitalWrite(LEFT_MOTOR_IN2, LOW);
-  
+
   delay(100);
   RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 
-  
+
   // digitalWrite(RIGHT_MOTOR_IN1, HIGH);
   // digitalWrite(RIGHT_MOTOR_IN2, LOW);
   // digitalWrite(LEFT_MOTOR_IN1, HIGH);
